6b8d21f3f7b51893cf414a8c1762bc60d28ef2cb
moreno27 <alejanmm2000@gmail.com>
2021-09-29 12:29:13 +0200

    comienzo del ejercicio numeroALetra

---
+++ Recursividad/numeroALetra/main.js
@@ -0,0 +1,18 @@
+/*Crea una función recursiva numeroAletras() que convierta un número dado en letras.*/
+
+//VARIABLE NECESARIA
+var letras = ["uno", "dos", "tres", "cuatro", "cinco", "seis", "siete", "ocho", "nueve"];
+var numeroAlReves = [];
+
+/**
+ * 
+ * @param {Number} numero Numero que queremos paras a letra
+ * @returns texto con el numero
+ */
+function numeroAletras(numero) {
+
+
+    convertir(numero % 10)
+}
+
+numeroAletras(546)
