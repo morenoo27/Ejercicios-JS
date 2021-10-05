4c05504b96aa2cc84c720f012941ccf8cc6674e2
moreno27 <alejanmm2000@gmail.com>
2021-10-05 12:57:28 +0200

    inicios de Os

---
+++ Recursividad/Os/main.js
@@ -0,0 +1,95 @@
+//VARIABLES NECESARIAS
+var Os;
+var base = 0;
+
+//instancia de listeners
+document.getElementById("genSec").addEventListener("click", crearSecuencia, false);
+document.getElementById("BuscSec").disabled = true;
+document.getElementById("BuscSec").addEventListener("click", buscar, false);
+
+/**
+ * Metodo que genera un aleatorio entre 0 y 1.
+ * Para determinar si la letra sera una S u O
+ * @returns O | S
+ */
+function generarLetra() {
+
+    // devuelve o 0 o 1
+    if (Math.floor((Math.random() * (2))) == 1) {
+        return "S";
+    } else {
+        return "O";
+    }
+}
+
+/**
+ * metodo que genera un array con Os y Ss en su interior,
+ * con las letras generadas de manera aleatoria 
+ * @returns Array con O y S de manera aleatoria
+ */
+function generarOs() {
+
+    let longitud = 20;
+    Os = new Array();
+
+    for (let i = 0; i < longitud; i++) {
+        Os.push(generarLetra())
+    }
+
+    return Os;
+}
+
+function crearSecuencia() {
+    document.getElementById("Array").innerHTML = generarOs();
+    document.getElementById("BuscSec").disabled = false;
+}
+
+function buscar() {
+
+    let inicio = buscarMasLarga(base, base)
+    let longitud = obtenerLongitud(inicio);
+
+    document.getElementById("Respuesta").innerHTML = `La secuencia mas larda es de ${longitud} y empieza en la posicion ${inicio}`;
+}
+
+function buscarMasLarga(base, length) {
+
+    let inicio, longitud;
+    let masLarga = false;
+
+    for (let i = base; i < Os.length; i++) {
+
+        if (i == Os.length - 1) {
+            masLarga = true;
+            break;
+        }
+        if (Os[i] == "O") {
+
+            inicio = i;
+            longitud = obtenerLongitud(inicio);
+            break;
+        }
+    }
+
+    if (longitud > length) {
+        masLarga = true;
+    }
+
+    if (!masLarga) {
+        buscarMasLarga(inicio + 1, longitud);
+    } else {
+        return inicio;
+    }
+}
+
+function obtenerLongitud(inicio) {
+    let longitud = 0;
+    for (inicio; inicio < Os.length; inicio++) {
+
+        if (Os[inicio] == "S") {
+            break;
+        }
+        longitud++;
+    }
+    return longitud;
+}
