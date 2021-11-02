624970d457a6ed019fc84ad65ddabc6f213f30aa
moreno27 <alejanmm2000@gmail.com>
2021-11-02 12:37:25 +0100

    reto7

---
+++ tema 3/retos/reto7/main.js
@@ -0,0 +1,4 @@
+let frutas = ["aguacate", "pera", "manzana", "naranja", "tomate", "granada", "mango", "litchi", "nispero"];
+let tropicStock = [{ fruta: "aguacate", stock: 50 }, { fruta: "mango", stock: 10 }, { fruta: "litchi", stock: 20 }, { fruta: "nispero", stock: 25 }];
+// muestra las frutas tropicales con stock menor a 40 como elementos li y su nodo padre será el ul frutas
+
