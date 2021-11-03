284a122d6dd9a9eaa50c1af18e57d20306ed6618
moreno27 <alejanmm2000@gmail.com>
2021-11-03 11:51:30 +0100

    inicio reto 8

--- tema 3/retos/reto7/main.js
+++ tema 3/retos/reto7/main.js
@@ -1,4 +1,13 @@
-let frutas = ["aguacate", "pera", "manzana", "naranja", "tomate", "granada", "mango", "litchi", "nispero"];
-let tropicStock = [{ fruta: "aguacate", stock: 50 }, { fruta: "mango", stock: 10 }, { fruta: "litchi", stock: 20 }, { fruta: "nispero", stock: 25 }];
+let tropicStock = [{ fruta: "aguacate", stock: 50 }, { fruta: "mango", stock: 10 }, { fruta: "litchi", stocsk: 20 }, { fruta: "nispero", stock: 25 }];
 // muestra las frutas tropicales con stock menor a 40 como elementos li y su nodo padre será el ul frutas
 
+let ul = document.getElementById("frutas");
+tropicStock.filter(obj => obj.stock < 50).map(fruta => document.getElementById("frutas").appendChild(document.createElement("li").appendChild(document.createTextNode(fruta.fruta))))
+
+//document.getElementById("frutas").appendChild(document.createElement("li").appendChild(document.createTextNode(fruta.fruta)))
+/*{
+    let li = document.createElement("li");
+    let textoLi = document.createTextNode(fruta.fruta);
+    li.appendChild(textoLi);
+    ul.appendChild(li);
+}*/
