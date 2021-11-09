0f5f3bf7c32821a0fc9bf8e12a945455556d88fc
Moreno27 <alejanmm2000@gmail.com>
2021-11-09 20:51:54 +0100

    reto 12

---
+++ tema 3/retos/reto12/main.js
@@ -0,0 +1,3 @@
+let carrakos = ["Chevrolet", "Tesla", "BMW", ["Honda", "Infiniti", ["Lexus", "Mercedes", "Kia"], "Hyunday"], ["Mazda", "Mercedes", "Jeep", ["KTM", "Nissan", "Isuzu"]]];
+let xungos = [{ nombre: "Bruce Lee", carros: ["Chevrolet", "Tesla", "BMW"] }, { nombre: "Steven Seagal", carros: ["Chevrolet", "Mazda", "Jeep", "Nissan"] }, { nombre: "YakiChan", carros: ["Lexus", "Mercedes", "Kia"] }]
+
